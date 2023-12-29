c2e275334bf67456be35af979105a5c59fdcd4d2
Elias Croze <elias.croze@datadoghq.com>
2023-12-29 14:51:43 +0100

    Implemented actual check

--- src/main/java/org/ecroze/Main.java
+++ src/main/java/org/ecroze/Main.java
@@ -10,11 +10,23 @@ public class Main {
   public static class Solver {
     private static final Pattern VALID_STRING = Pattern.compile("[0-1]*");
 
-    public static boolean isSolvable(String s) {
-      if (!VALID_STRING.matcher(s).matches()) {
+    public static boolean isSolvable(String input) {
+      if (!VALID_STRING.matcher(input).matches()) {
         throw new IllegalArgumentException("String must contain characters 0 and 1 only");
       }
-      return false;
+
+      // If we only have ones, we cannot add any zero because '11' gives '111' after insertion (-> NO answer)
+      // If we have at least one 0, either we only have zeroes (-> YES answer)
+      // Or we have the substring '10' or '01' somewhere in the string
+      // '10' gives '100' which means we can repeat the insertion as many times until we have a majority of zeroes.
+      // '01' gives '001' which means we can repeat the insertion as many times until we have a majority of zeroes.
+
+      // So the test is: we check we have at least one zero somewhere in the string.
+      return input.indexOf('0') >= 0;
+    }
+
+    private static long getNumberOfZeroes(String input) {
+      return input.chars().filter(c -> c == '0').count();
     }
   }
 
--- src/test/java/org/ecroze/SolverTest.java
+++ src/test/java/org/ecroze/SolverTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SolverTest {
 
@@ -23,5 +24,22 @@ class SolverTest {
   @ValueSource(strings = {"1", "11", "111"})
   void testStringWithOnlyOnes(String input) {
     assertFalse(Main.Solver.isSolvable(input));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"0", "000", "01010", "11000"})
+  void testStringWithAlreadyEnoughZeroes(String input) {
+    assertTrue(Main.Solver.isSolvable(input));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"11111011111", "0111111", "1111110"})
+  void testStringWithOneZero(String input) {
+    assertTrue(Main.Solver.isSolvable(input));
+  }
+
+  @Test
+  void testStringWithSeveralZeroes() {
+    assertTrue(Main.Solver.isSolvable("11011101111"));
   }
 }
